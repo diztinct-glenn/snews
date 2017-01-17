ce910a2de0225f37948f6dac2f302d6a2951f6d4
Glenn Basgaard <glenn_basgaard@yahoo.com>
2017-01-16 21:56:35 -0500

    create news_source module and help parameter for slash command

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 3000;
 const CLIENT_ID = process.env.SLACK_CLIENT_ID;
 const CLIENT_SECRET = process.env.SLACK_CLIENT_SECRET;
 const NEWS_API_KEY = process.env.NEWS_API_KEY;
+const news_sources = require('./news_sources');
 
 app.use("/", express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -26,35 +27,42 @@ app.get("/snews", function(req, res) {
 app.post("/snews", function(req,res) {
   input = req.body.text;
   console.log(input);
+  const api = `https://newsapi.org/v1/articles?source=${input}&sortBy=top&apiKey=${NEWS_API_KEY}`;
 
-  var api = `https://newsapi.org/v1/articles?source=${input}&sortBy=top&apiKey=${NEWS_API_KEY}`
-  request(api, function(err, resp, body){
-      body = JSON.parse(body);
-      // console.log(body);
-      source = body.source.toUpperCase();
-      articles = body.articles[0].title;
-      link = body.articles[0].url;
-      description = body.articles[0].description;
-      image = body.articles[0].urlToImage;
-      console.log(source, articles);
+  if(input === "help") {
 
-      slack_message = {
-        attachments: [
-          {
-            color: "#ff0000",
-            pretext: source,
-            title: articles,
-            title_link: link,
-            text: description,
-            thumb_url: image,
-            footer: "snews",
-            footer_icon: "http://emojipedia-us.s3.amazonaws.com/cache/a3/dd/a3dd2044fded090033553d2c6a893d82.png"
-          }
-        ]
+    slack_message = news_sources;
+    res.send (slack_message);
 
-      }
+  } else {
 
-      res.send(slack_message);
-  })
-  // res.send(req.body)
+    request(api, function(err, resp, body){
+        body = JSON.parse(body);
+        // console.log(body);
+        source = body.source.toUpperCase();
+        articles = body.articles[0].title;
+        link = body.articles[0].url;
+        description = body.articles[0].description;
+        image = body.articles[0].urlToImage;
+        console.log(source, articles);
+
+        slack_message = {
+          attachments: [
+            {
+              color: "#ff0000",
+              pretext: source,
+              title: articles,
+              title_link: link,
+              text: description,
+              thumb_url: image,
+              footer: "snews",
+              footer_icon: "http://emojipedia-us.s3.amazonaws.com/cache/a3/dd/a3dd2044fded090033553d2c6a893d82.png"
+            }
+          ]
+
+        }
+
+        res.send(slack_message);
+    })
+  }
 });
---
+++ news_sources.js
@@ -0,0 +1,209 @@
+module.exports = {
+  attachments: [
+    {
+      color: "#ff0000",
+      pretext: "Here's a list of all supported news sources:",
+      text: "Underneath each source is the correct input to type after the /snews command.",
+      footer: "snews",
+      footer_icon: "http://emojipedia-us.s3.amazonaws.com/cache/a3/dd/a3dd2044fded090033553d2c6a893d82.png",
+      fields: [
+        {
+            title: "Ars Technica",
+            value: "ars-technica"
+        },
+        {
+            title: "Associated Press",
+            value: "associated-press"
+        },
+        {
+            title: "BBC News",
+            value: "bbc-news"
+        },
+        {
+            title: "BBC Sports",
+            value: "bbc-sports"
+        },
+        {
+            title: "Bloomberg",
+            value: "bloomberg"
+        },
+        {
+            title: "Business Insider",
+            value: "business-insider"
+        },
+        {
+            title: "Buzzfeed",
+            value: "buzzfeed"
+        },
+        {
+            title: "CNBC",
+            value: "cnbc"
+        },
+        {
+            title: "CNN",
+            value: "cnn"
+        },
+        {
+            title: "Daily News",
+            value: "daily-news"
+        },
+        {
+            title: "Engadget",
+            value: "engadget"
+        },
+        {
+            title: "Entertainment Weekly",
+            value: "entertainment-weekly"
+        },
+        {
+            title: "ESPN",
+            value: "espn"
+        },
+        {
+            title: "Financial Times",
+            value: "financial-times"
+        },
+        {
+            title: "FourFourTwo",
+            value: "four-four-two"
+        },
+        {
+            title: "Fox Sports",
+            value: "fox-sports"
+        },
+        {
+            title: "Google News",
+            value: "google-news"
+        },
+        {
+            title: "Hacker News",
+            value: "hacker-news"
+        },
+        {
+            title: "IGN",
+            value: "ign"
+        },
+        {
+            title: "Independent",
+            value: "independent"
+        },
+        {
+            title: "Mashable",
+            value: "mashable"
+        },
+        {
+            title: "Metro",
+            value: "metro"
+        },
+        {
+            title: "Mirror",
+            value: "mirror"
+        },
+        {
+            title: "MTV News",
+            value: "mtv-news"
+        },
+        {
+            title: "National Geographic",
+            value: "national-geographic"
+        },
+        {
+            title: "New Scientist",
+            value: "new-scientist"
+        },
+        {
+            title: "Newsweek",
+            value: "newsweek"
+        },
+        {
+            title: "New York Magazine",
+            value: "new-york-magazine"
+        },
+        {
+            title: "NFL News",
+            value: "nfl-news"
+        },
+        {
+            title: "Polygon",
+            value: "polygon"
+        },
+        {
+            title: "Recode",
+            value: "recode"
+        },
+        {
+            title: "Reddit /r/all",
+            value: "reddit-r-all"
+        },
+        {
+            title: "Reuters",
+            value: "reuters"
+        },
+        {
+            title: "Sky News",
+            value: "sky-news"
+        },
+        {
+            title: "Sky Sports News",
+            value: "sky-sports-news"
+        },
+        {
+            title: "TechCrunch",
+            value: "techcrunch"
+        },
+        {
+            title: "TechRadar",
+            value: "techradar"
+        },
+        {
+            title: "The Economist",
+            value: "the-economist"
+        },
+        {
+            title: "The Guardian (UK)",
+            value: "the-guardian-uk"
+        },
+        {
+            title: "The Huffington Post",
+            value: "the-huffington-post"
+        },
+        {
+            title: "The Lad Bible",
+            value: "the-lad-bible"
+        },
+        {
+            title: "The New York Times",
+            value: "the-new-york-times"
+        },
+        {
+            title: "The Next Web",
+            value: "the-next-web"
+        },
+        {
+            title: "The Telegraph",
+            value: "the-telegraph"
+        },
+        {
+            title: "The Verge",
+            value: "the-verge"
+        },
+        {
+            title: "The Wall Street Journal",
+            value: "the-wall-street-journal"
+        },
+        {
+            title: "The Washington Post",
+            value: "the-washington-post"
+        },
+        {
+            title: "Time",
+            value: "time"
+        },
+        {
+            title: "USA Today",
+            value: "usa-today"
+        }
+      ]
+    }
+  ]
+}
