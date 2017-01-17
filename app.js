e6c106073c3cf23e4d646ef9c6c18d362921fdd4
Glenn Basgaard <glenn_basgaard@yahoo.com>
2017-01-16 19:12:11 -0500

    add message formatting

--- app.js
+++ app.js
@@ -24,25 +24,37 @@ app.get("/snews", function(req, res) {
 });
 
 app.post("/snews", function(req,res) {
-  // input = JSON.stringify(req.body);
   input = req.body.text;
   console.log(input);
 
-  var api = `https://newsapi.org/v1/articles?source=${input}&sortBy=latest&apiKey=${NEWS_API_KEY}`
+  var api = `https://newsapi.org/v1/articles?source=${input}&sortBy=top&apiKey=${NEWS_API_KEY}`
   request(api, function(err, resp, body){
-      // console.log(body) // Logs JSON object from the external API of the specific beer I searched
       body = JSON.parse(body);
-      console.log(body);
-      source = body.source;
+      // console.log(body);
+      source = body.source.toUpperCase();
       articles = body.articles[0].title;
+      link = body.articles[0].url;
+      description = body.articles[0].description;
+      image = body.articles[0].urlToImage;
       console.log(source, articles);
 
       slack_message = {
-        text: input
+        attachments: [
+          {
+            color: "#ff0000",
+            pretext: source,
+            title: articles,
+            title_link: link,
+            text: description,
+            thumb_url: image,
+            footer: "snews",
+            footer_icon: "http://emojipedia-us.s3.amazonaws.com/cache/a3/dd/a3dd2044fded090033553d2c6a893d82.png"
+          }
+        ]
 
       }
 
-      res.send(body.articles); // sends json object back to front end
+      res.send(slack_message);
   })
   // res.send(req.body)
 });
