1889b7d3e106f99da7ab8f2df781a05e854fe9ee
Glenn Basgaard <glenn_basgaard@yahoo.com>
2017-01-20 15:26:57 -0500

    clean up code

--- app.js
+++ app.js
@@ -27,7 +27,6 @@ app.get("/", (req, res) =>
 app.post("/snews", function(req,res) {
   const input = req.body.text;
   let sorting = "top";
-  console.log(input);
   let api = `https://newsapi.org/v1/articles?source=${input}&sortBy=${sorting}&apiKey=${NEWS_API_KEY}`;
   if(input === "something") {
     res.send("Sorry, I didn't quite catch that. Try using the command [help] to see a list of acceptable commands!")
@@ -40,11 +39,9 @@ app.post("/snews", function(req,res) {
        sorting = "latest";
        api = `https://newsapi.org/v1/articles?source=${input}&sortBy=${sorting}&apiKey=${NEWS_API_KEY}`;
        request(api, function(err, resp, body){
-        body = JSON.parse(body)
-        // console.log(body);
+        body = JSON.parse(body);
         const source = body.source.toUpperCase();
         const articles_new = body.articles;
-        // console.log(articles_new)
 
         let tester_art = articles_new.map(function(element) {
           let article_title = element.title;
@@ -99,7 +96,6 @@ app.post("/snews", function(req,res) {
               footer_icon: "http://emojipedia-us.s3.amazonaws.com/cache/a3/dd/a3dd2044fded090033553d2c6a893d82.png"
             }
         })
-        // console.log(tester_art);
 
         slack_message = {
           text: `*${source}*`,
@@ -107,7 +103,7 @@ app.post("/snews", function(req,res) {
           mrkdwn_in: "text",
           attachments: tester_art
         }
-        // console.log(slack_message)
+
         res.send(slack_message);
     })
   }
@@ -116,18 +112,15 @@ app.post("/snews", function(req,res) {
 app.get("/slack", function(req,res) {
   let data = {
     form: {
-    client_id: SLACK_CLIENT_ID,
-    client_secret: SLACK_CLIENT_SECRET,
-    code: req.query.code
+      client_id: SLACK_CLIENT_ID,
+      client_secret: SLACK_CLIENT_SECRET,
+      code: req.query.code
+    }
   }
-}
-  console.log(req.query.code);
 
   request.post('https://slack.com/api/oauth.access', data, function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      // You are done.
-      // If you want to get team info, you need to get the token here
-      let token = JSON.parse(body).access_token; // Auth token
+      let token = JSON.parse(body).access_token;
       res.redirect('https://snews-app.herokuapp.com/');
     }
   })
